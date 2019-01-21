8ddfc4efdcd37ce61b5737d7e2a21252ebf83a8c
jcid <jcid.rojas@gmail.com>
2019-01-21 13:02:48 -0800

    Added answer table migration

---
+++ src/database/migrations/2019_01_21_203543_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("comments")->nullable();
+            $table->string("correct")->nullable();
+            $table->integer("speed")->nullable();
+            $table->unsignedInteger("user_id");
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
